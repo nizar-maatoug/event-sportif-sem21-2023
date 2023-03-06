3c7da4c4f01fdb2956300bed2ff0843498258022
nizar-maatoug <nizar.maatoug12@gmail.com>
2023-03-06 11:35:50 +0100

    version 1

---
+++ src/main/java/org/isetbz/eventsportif/EventSportifSem212023Application.java
@@ -0,0 +1,13 @@
+package org.isetbz.eventsportif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSportifSem212023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSportifSem212023Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/isetbz/eventsportif/EventSportifSem212023ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isetbz.eventsportif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSportifSem212023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
