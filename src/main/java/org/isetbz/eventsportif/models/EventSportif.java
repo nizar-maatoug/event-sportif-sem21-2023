84618fe326849e7382c3dfdc22fda9b23166421e
nizar-maatoug <nizar.maatoug12@gmail.com>
2023-03-07 07:20:51 +0100

    version 3

--- src/main/java/org/isetbz/eventsportif/models/EventSportif.java
+++ src/main/java/org/isetbz/eventsportif/models/EventSportif.java
@@ -18,5 +18,62 @@ public class EventSportif implements Serializable {
 
     private Date dateFin;
 
+    public EventSportif() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getLieu() {
+        return Lieu;
+    }
+
+    public void setLieu(String lieu) {
+        Lieu = lieu;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
 }
