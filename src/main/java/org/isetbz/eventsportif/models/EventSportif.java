137a8d985fe1d717ad7da19b34ca69b14bb40cf0
nizar-maatoug <nizar.maatoug12@gmail.com>
2023-03-07 07:19:37 +0100

    version 2

---
+++ src/main/java/org/isetbz/eventsportif/models/EventSportif.java
@@ -0,0 +1,22 @@
+package org.isetbz.eventsportif.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class EventSportif implements Serializable {
+    private Long id;
+    private String nom;
+
+    private String Lieu;
+
+    private String logo;
+
+    private String poster;
+
+    private Date dateDebut;
+
+    private Date dateFin;
+
+
+}
