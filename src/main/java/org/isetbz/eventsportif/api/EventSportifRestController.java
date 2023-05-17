cbedf2efa70a0895117df7fe44f11ca2a0de241b
nizar-maatoug <nizar.maatoug12@gmail.com>
2023-05-18 00:01:57 +0100

    REST API

---
+++ src/main/java/org/isetbz/eventsportif/api/EventSportifRestController.java
@@ -0,0 +1,64 @@
+package org.isetbz.eventsportif.api;
+
+import org.isetbz.eventsportif.dto.EventSportifDTO;
+import org.isetbz.eventsportif.services.EventSportifService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/eventSportifs")
+public class EventSportifRestController {
+
+    EventSportifService eventSportifService;
+
+    public EventSportifRestController(EventSportifService eventSportifService) {
+        this.eventSportifService = eventSportifService;
+    }
+
+    //Create EventSportif
+    @PostMapping
+    public ResponseEntity<EventSportifDTO> createEventSportif(@RequestBody EventSportifDTO eventSportifDTO){
+        EventSportifDTO savedEventSportifDTO=eventSportifService.save(eventSportifDTO);
+
+        return new ResponseEntity<>(savedEventSportifDTO,HttpStatus.CREATED);
+    }
+
+    //Get EventSportif by Id
+    @GetMapping("{id}")
+    public ResponseEntity<EventSportifDTO> getEventSportifById(@PathVariable("id") Long id){
+        EventSportifDTO eventSportifDTO=eventSportifService.getById(id);
+        return new ResponseEntity<>(eventSportifDTO,HttpStatus.OK);
+    }
+
+    //Get all EventSportifs
+
+    @GetMapping
+    public ResponseEntity<List<EventSportifDTO>> getAllEvents(){
+        List<EventSportifDTO> eventSportifDTOS=eventSportifService.findAll();
+
+        return new ResponseEntity<>(eventSportifDTOS,HttpStatus.OK);
+    }
+
+    //Update EventSportif
+    @PutMapping("{id}")
+    public ResponseEntity<EventSportifDTO> updateEvent(@PathVariable("id") Long id,
+            @RequestBody EventSportifDTO eventSportifDTO){
+        eventSportifDTO.setId(id);
+        EventSportifDTO updatedEventSportifDTO=eventSportifService.update(eventSportifDTO);
+        return new ResponseEntity<>(updatedEventSportifDTO,HttpStatus.OK);
+    }
+
+    //Delete EventSportif by id
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEventById(@PathVariable("id") Long id){
+        eventSportifService.deleteById(id);
+
+        return new ResponseEntity<>("EventSportif Deleted",HttpStatus.OK);
+    }
+
+
+}
